5ef82b72658d268eb4f52c297fe63473638876de
DennisYun <2008dennisyun@gmail.com>
2025-09-05 20:37:22 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+const __dirname = path.resolve();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/pages/index.html');
+});
+
+app.listen(port);
